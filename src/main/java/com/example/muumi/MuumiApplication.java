b299e71643b59083442e0374e0d8c6b3b21e7913
marikben <70907667+marikben@users.noreply.github.com>
2020-11-14 01:19:02 +0200

    Update MuumiApplication.java

--- src/main/java/com/example/muumi/MuumiApplication.java
+++ src/main/java/com/example/muumi/MuumiApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.example.muumi.model.KategoriaRepository;
+import com.example.muumi.model.Kategoriat;
 import com.example.muumi.model.MuumiRepository;
 import com.example.muumi.model.User;
 import com.example.muumi.model.UserRepository;
@@ -17,11 +19,14 @@ public class MuumiApplication {
 	}
 	
 	@Bean
-	public CommandLineRunner demo(UserRepository urepository){return(args) ->{
-		User user1 = new User("user","$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6","USER");
-		User user2 = new User("admin","$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C","ADMIN");
-		urepository.save(user1);
-		urepository.save(user2);
+	public CommandLineRunner demo(UserRepository urepository, KategoriaRepository krepository){return(args) ->{
+		//User user1 = new User("user","$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6","USER");
+		//User user2 = new User("admin","$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C","ADMIN");
+		//urepository.save(user1);
+		//urepository.save(user2);
+		krepository.save(new Kategoriat("Muki"));
+		krepository.save(new Kategoriat("Lautanen"));
+		krepository.save(new Kategoriat("Kulho"));
 	};
 	
 }
