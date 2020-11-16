dd8bc3072790919e06eec6563c7221c3095c59f3
marikben <70907667+marikben@users.noreply.github.com>
2020-11-16 19:12:51 +0200

    Update WebSecurityConfig.java

--- src/main/java/com/example/muumi/WebSecurityConfig.java
+++ src/main/java/com/example/muumi/WebSecurityConfig.java
@@ -23,6 +23,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     	http
         .authorizeRequests().antMatchers("/css/**","rescources/**").permitAll() 
         .and()
+        .authorizeRequests().antMatchers("/signup", "/saveuser").permitAll()
+        .and()
         .authorizeRequests()
           .anyRequest().authenticated()
           .and()
