a404066c8a1ef41713cbde1c1184dcfc0969f683
marikben <70907667+marikben@users.noreply.github.com>
2020-11-19 01:09:27 +0200

    Update MuumiRepository.java

--- src/main/java/com/example/muumi/model/MuumiRepository.java
+++ src/main/java/com/example/muumi/model/MuumiRepository.java
@@ -4,6 +4,6 @@ import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 public interface MuumiRepository extends CrudRepository <Muumi,Long>{
-	List<Muumi> findByNimi(String nimi);
+	
 
 }
