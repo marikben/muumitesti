4f99ff0315b5ade58f8601ea655fcc30cd0fffba
marikben <70907667+marikben@users.noreply.github.com>
2020-11-19 10:56:53 +0200

    Update MuumiApplicationTests.java

--- src/test/java/com/example/muumi/MuumiApplicationTests.java
+++ src/test/java/com/example/muumi/MuumiApplicationTests.java
@@ -3,6 +3,7 @@ package com.example.muumi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class MuumiApplicationTests {
 
