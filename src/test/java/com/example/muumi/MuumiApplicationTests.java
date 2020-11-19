005b644cc4d45fe1fcaa55022ba759fd46e09c6a
marikben <70907667+marikben@users.noreply.github.com>
2020-11-19 11:10:16 +0200

    Update MuumiApplicationTests.java

--- src/test/java/com/example/muumi/MuumiApplicationTests.java
+++ src/test/java/com/example/muumi/MuumiApplicationTests.java
@@ -1,11 +1,12 @@
 package com.example.muumi;
 
-import org.junit.runner.RunWith;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+//Commented out because of Heroku
+//import org.junit.runner.RunWith;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 class MuumiApplicationTests {
 
 	@Test
