711b58397d58e09a493d72d29b7d506340c90726
marikben <70907667+marikben@users.noreply.github.com>
2020-11-19 10:54:48 +0200

    Add files via upload

---
+++ src/test/java/com/example/muumi/MuumiRepositoryTests.java
@@ -0,0 +1,40 @@
+package com.example.muumi;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.muumi.model.KategoriaRepository;
+import com.example.muumi.model.Kategoriat;
+import com.example.muumi.model.Muumi;
+import com.example.muumi.model.MuumiRepository;
+import com.example.muumi.web.MuumiController;
+
+public class MuumiRepositoryTests {
+	@Autowired 
+	private MuumiController mcontroller;
+	@Autowired
+	private KategoriaRepository krepository;
+	@Autowired
+	private MuumiRepository mrepository;
+		//Testing that the context is creating the MuumiController
+		@Test
+		public void controllerLoads() throws Exception {
+			assertThat(mcontroller).isNotNull();
+		}
+		@Test
+		public void createNewMuumi() {
+			Muumi muumi = new Muumi("Testimuki",krepository.findByName("Muki").get(0),"https://www.ikea.com/fi/fi/images/products/vaerdera-muki__0711121_PE727989_S5.JPG?f=s");
+			mrepository.save(muumi);
+			assertThat(muumi.getId()).isNotNull();
+		}
+	    @Test
+	    public void findByNameShouldReturnMuumi() {
+	        List<Muumi> muumis = mrepository.findByNimi("Mörkö");
+	        assertThat(muumis).hasSize(1);
+	        assertThat(muumis.get(0).getNimi()).isEqualTo("Mörkö");
+	    }
+}
